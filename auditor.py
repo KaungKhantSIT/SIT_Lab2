c7af513a9e1b5957ed82abf4c6b64755b0a7a2af
Kaung Khant <2602036@sit.singaporetech.edu.sg>
2026-09-11 11:43:52 +0800

    Refined exception handling, added -ve number handling

--- auditor.py
+++ auditor.py
@@ -4,9 +4,17 @@ user = ""
 
 while user.lower() != "quit":
     user = input("Enter stock quantity: ")
-    if user != "quit":
-        if user.isdigit():
+    try:
+        if user.lower() == "quit":
+            break
+        if int(user) >= 0:
             stock = int(user)
+            print("Current Stock:",stock)
         else:
-            print("Please enter a number.")
+            print("Please enter a positive number.")
+    except ValueError:
+        print("Please enter a number.")
+
+    
+
 
