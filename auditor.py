c327e971beedec4b2a2737ca011523988ff195c5
Kaung Khant <2602036@sit.singaporetech.edu.sg>
2026-09-11 11:48:35 +0800

    Added total stock & failed entries report after quitting

--- auditor.py
+++ auditor.py
@@ -1,18 +1,23 @@
 #Vars
 stock = 0
 user = ""
+fails = 0
 
 while user.lower() != "quit":
     user = input("Enter stock quantity: ")
     try:
         if user.lower() == "quit":
+            print("Total Units Processed:",stock)
+            print("Number of Failed/Rejected Entries:",fails)
             break
         if int(user) >= 0:
             stock = int(user)
             print("Current Stock:",stock)
         else:
+            fails += 1
             print("Please enter a positive number.")
     except ValueError:
+        fails += 1
         print("Please enter a number.")
 
     
